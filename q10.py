dfb00087f60c8b85badb20b3c54acaea529e1dbd
niti-sha <nitishakhh@gmail.com>
2022-07-27 14:46:58 +0530

    first commit

---
+++ q10.py
@@ -0,0 +1,7 @@
+lst = [10, 11, 12, 13, 14, 15]
+l = []  # empty list
+for i in lst:
+    # reversing the list
+    l.insert(0, i)
+
+print(l)
---
+++ q18.py
@@ -0,0 +1,15 @@
+def average_tuple(nums):
+    result = tuple(map(lambda x: sum(x) / float(len(x)), zip(*nums)))
+    return result
+
+
+nums = ((10, 10, 10, 12), (30, 45, 56, 45), (81, 80, 39, 32), (1, 2, 3, 4))
+print("Original Tuple: ")
+print(nums)
+print("\nAverage value of the numbers of the said tuple of tuples:\n",
+      average_tuple(nums))
+nums = ((1, 1, -5), (30, -15, 56), (81, -60, -39), (-10, 2, 3))
+print("\nOriginal Tuple: ")
+print(nums)
+print("\nAverage value of the numbers of the said tuple of tuples:\n",
+      average_tuple(nums))
